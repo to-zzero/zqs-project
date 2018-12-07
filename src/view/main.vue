a5912b556c5bc1fd8a0cb5c0b3d39eb25313e877
zqs <18611433407@163.com>
2018-12-07 17:03:44 +0800

    新建项目

---
+++ src/view/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="zqs-main">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'zqs_main',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
