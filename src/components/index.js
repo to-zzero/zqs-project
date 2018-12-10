89e7a4f5eabd21b36fd0e952962f21b743793a28
zqs <18611433407@163.com>
2018-12-10 18:10:14 +0800

    过渡组件

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
+import MyTransition from './my-transition'
 import MyImg from './my-img'
 
+Vue.component('my-transition', MyTransition)
 Vue.component('my-img', MyImg)
--- src/components/my-img.vue
+++ src/components/my-img.vue
@@ -1,17 +1,33 @@
 <template>
   <!-- 图片统一错误处理 -->
-  <img
-    @click="$emit('click')"
-    :src="realSrc"
-    :alt="alt"
-    @error="error"
-    class="my-img">
+  <div>
+    <my-transition
+      v-if="loading">
+      <div class="img-loading"></div>
+    </my-transition>
+    <my-transition
+      v-else-if="showImg">
+      <img
+        @click="$emit('click')"
+        :src="defaultSrc || realSrc"
+        :alt="alt"
+        @error="error"
+        class="my-img">
+    </my-transition>
+  </div>
 </template>
 
 <script>
 import {mapState} from 'vuex'
 export default {
   name: 'my-img',
+  data () {
+    return {
+      defaultSrc: '',
+      loading: true,
+      showImg: false
+    }
+  },
   props: {
     src: {
       type: String,
@@ -38,10 +54,49 @@ export default {
       }
     }
   },
+  created () {
+    let img = document.createElement('img')
+    img.src = this.realSrc
+    img.onload = () => {
+      this.loading = false
+      this.showImg = true
+    }
+  },
   methods: {
     error (err) {
+      this.defaultSrc = '/static/theme_img/dark/v2-2dbef9e766899ebe9f80cc919a3ebb08_b.jpg'
       console.log(err)
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+  .img-loading {
+    border-radius: 4px;
+    width: inherit;
+    height: inherit;
+    box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.08), 0 15px 35px 0 rgba(0, 0, 0, 0.16);
+    @include bgColor_rgba(a);
+    transition: all .5s;
+    position: absolute;
+    &::after {
+      position: absolute;
+      content: '';
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      @include borderColor_rgba(3px, solid, b, left, .8);
+      @include borderColor_rgba(3px, solid, b, right, .8);
+      @include borderColor_rgba(3px, solid, b, bottom, .8);
+      @include borderColor_rgba(3px, solid, b, top, 0);
+      animation: rotate 2s linear infinite;
+      top: calc(50% - 10px);
+      left: calc(50% - 10px);
+    }
+  }
+  .my-img {
+    width: inherit;
+    height: inherit;
+  }
+</style>
